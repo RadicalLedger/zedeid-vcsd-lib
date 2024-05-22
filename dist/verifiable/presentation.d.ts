1b53255e6ad82bed24a75bfe35bbf6b6a3fa93ea
Sadeepa Dhananjaya <sadeepaekanayaked@gmail.com>
2024-05-22 17:06:13 +0530

    multiple did methods

--- dist/verifiable/presentation.d.ts
+++ dist/verifiable/presentation.d.ts
@@ -2,6 +2,6 @@ import { VerifiablePresentation } from '@transmute/vc.js/dist/types/VerifiablePr
 import { CreateProps, VerifyProps } from '../types/presentation.type';
 declare const _default: {
     create: ({ suite, challenge, issuanceDate, domain, documentLoader, holderPrivateKey, holderDID, verifiableCredential, masks, didMethod }: CreateProps) => Promise<VerifiablePresentation>;
-    verify: ({ suite, challenge, domain, vp, documentLoader, issuerPublicKey, holderPublicKey, didMethod }: VerifyProps) => Promise<import("@transmute/vc.js/dist/types").VerificationResult>;
+    verify: ({ suite, challenge, domain, vp, documentLoader, issuerPublicKey, holderPublicKey }: VerifyProps) => Promise<import("@transmute/vc.js/dist/types").VerificationResult>;
 };
 export default _default;
