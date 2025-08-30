d7b116bcafe8250c495678b7aeb6656520198c0b
Terance Edmonds <teranceedmonds20@gmail.com>
2025-08-30 16:28:10 +0530

    util signature methods

--- dist/verifiable/credential.d.ts
+++
@@ -1,9 +0,0 @@
-import { VerifiableCredential } from '@transmute/vc.js/dist/types/VerifiableCredential';
-import { CreateProps, VerifyProps } from '../types/credential';
-declare const _default: {
-    create: ({ issuerPrivateKey, issuanceDate, documentLoader, credential, suite, didMethod }: CreateProps) => Promise<VerifiableCredential>;
-    verify: ({ suite, vc, documentLoader, issuerPublicKey, holderPublicKey, didMethod }: VerifyProps) => Promise<{
-        verified: any;
-    }>;
-};
-export default _default;
